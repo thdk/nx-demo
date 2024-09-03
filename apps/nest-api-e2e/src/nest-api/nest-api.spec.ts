b99905bbe63b8a367a18808aaca8493549bcf3ef
Thomas Dekiere <thomas.dekiere@inthepocket.com>
2024-09-03 12:56:42 +0200

    feat(nest-api): add books module

--- apps/nest-api-e2e/src/nest-api/nest-api.spec.ts
+++ apps/nest-api-e2e/src/nest-api/nest-api.spec.ts
@@ -8,3 +8,23 @@ describe('GET /api', () => {
     expect(res.data).toEqual({ message: 'Hello API' });
   });
 });
+
+describe('GET /api/books', () => {
+  it('should return a list of books', async () => {
+    const res = await axios.get(`/api/books`);
+
+    expect(res.status).toBe(200);
+    expect(res.data).toEqual([
+      { id: 1, title: 'The Great Gatsby' },
+      { id: 2, title: 'The Catcher in the Rye' },
+      { id: 3, title: 'To Kill a Mockingbird' },
+      { id: 4, title: '1984' },
+      { id: 5, title: 'The Lord of the Rings' },
+      { id: 6, title: 'Pride and Prejudice' },
+      { id: 7, title: 'The Book Thief' },
+      { id: 8, title: 'Harry Potter' },
+      { id: 9, title: 'The Hobbit' },
+      { id: 10, title: 'The Chronicles of Narnia' },
+    ]);
+  });
+});
---
+++ apps/nest-api/src/books/books.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('books')
+export class BooksController {
+  @Get()
+  async books() {
+    return [
+      { id: 1, title: 'The Great Gatsby' },
+      { id: 2, title: 'The Catcher in the Rye' },
+      { id: 3, title: 'To Kill a Mockingbird' },
+      { id: 4, title: '1984' },
+      { id: 5, title: 'The Lord of the Rings' },
+      { id: 6, title: 'Pride and Prejudice' },
+      { id: 7, title: 'The Book Thief' },
+      { id: 8, title: 'Harry Potter' },
+      { id: 9, title: 'The Hobbit' },
+      { id: 10, title: 'The Chronicles of Narnia' },
+    ];
+  }
+}
