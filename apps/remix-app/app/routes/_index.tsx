4a7346a8a34017f6cd459849596bb6aa9edbba18
Thomas Dekiere <thomas.dekiere@inthepocket.com>
2024-09-03 12:57:36 +0200

    feat(remix-app): use api-client lib to fetch data from api

--- apps/remix-app/app/routes/_index.tsx
+++ apps/remix-app/app/routes/_index.tsx
@@ -1,9 +1,19 @@
-import NxWelcome from '../nx-welcome';
+import { findBooks } from '@nx-demo/api-client';
+import { useLoaderData } from '@remix-run/react';
+
+export async function loader() {
+  return {
+    books: await findBooks(),
+  };
+}
 
 export default function Index() {
+  const books = useLoaderData<typeof loader>().books;
   return (
-    <div>
-      <NxWelcome title={'remix-app'} />
-    </div>
+    <ul>
+      {books.map((book) => (
+        <li key={book.id}>{book.title}</li>
+      ))}
+    </ul>
   );
 }
