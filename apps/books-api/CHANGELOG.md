6a2fa3acc26131d1f338300ee68b3f5efa7dec6e
Thomas Dekiere <thomas.dekiere@inthepocket.com>
2024-09-08 20:43:26 +0200

    chore: rename nest-api into books-api

--- apps/books-api/CHANGELOG.md
+++ apps/books-api/CHANGELOG.md
@@ -1,11 +1,9 @@
 ## 0.3.0 (2024-09-07)
 
-
 ### New Features
 
 - use tags to differentiate apps from packages
 
-
-### ❤️  Thank You
+### ❤️ Thank You
 
 - Thomas Dekiere
--- apps/books-api/jest.config.ts
+++ apps/books-api/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'nest-api',
+  displayName: 'books-api',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/nest-api',
+  coverageDirectory: '../../coverage/apps/books-api',
 };
