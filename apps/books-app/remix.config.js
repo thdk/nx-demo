568137f7557c7cd427f9e71ecc14114ddda16428
Thomas Dekiere <thomas.dekiere@inthepocket.com>
2024-12-28 19:20:32 +0100

    build: enable remix v3 feature flags

--- apps/books-app/remix.config.js
+++ apps/books-app/remix.config.js
@@ -13,5 +13,12 @@ export default {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  future: {
+    v3_fetcherPersist: true,
+    v3_lazyRouteDiscovery: true,
+    v3_relativeSplatPath: true,
+    v3_singleFetch: true,
+    v3_throwAbortReason: true,
+  },
   watchPaths: () => createWatchPaths(__dirname),
 };
