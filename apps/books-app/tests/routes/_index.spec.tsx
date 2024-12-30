0e6fbcba7e6665e117870ae1418f6cfb0b51d869
Thomas Dekiere <thomas.dekiere@inthepocket.com>
2024-12-30 22:36:42 +0100

    build: use vite instead of remix server

--- apps/books-app/tests/routes/_index.spec.tsx
+++ apps/books-app/tests/routes/_index.spec.tsx
@@ -1,18 +1,45 @@
-// import { createRemixStub } from '@remix-run/testing';
-// import { render } from '@testing-library/react';
-// import Index from '../../app/routes/_index';
+import { createRemixStub } from '@remix-run/testing';
+import { render, screen, waitFor } from '@testing-library/react';
+import Index, { loader } from '../../app/routes/_index';
+
+import { setupServer } from 'msw/node';
+import { http, HttpResponse } from 'msw';
+
+const books = [
+  {
+    id: '1',
+    title: 'The lord of the rings',
+  },
+  // ...
+];
+
+export const restHandlers = [
+  http.get('http://localhost:3000/api/books', () => {
+    return HttpResponse.json(books);
+  }),
+];
+
+const server = setupServer(...restHandlers);
+
+// Start server before all tests
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+
+//  Close server after all tests
+afterAll(() => server.close());
+
+// Reset handlers after each test `important for test isolation`
+afterEach(() => server.resetHandlers());
 
 test('renders loader data', async () => {
-  // TODO: add a real test
-  expect(true).toBe(true);
+  const RemixStub = createRemixStub([
+    {
+      path: '/',
+      Component: Index,
+      loader,
+    },
+  ]);
 
-  // const RemixStub = createRemixStub([
-  //   {
-  //     path: '/',
-  //     Component: Index,
-  //   },
-  // ]);
-  // render(<RemixStub />);
-  // TODO: mock @thdk/books-api-client findBooks function
-  // await waitFor(() => screen.findByText('The Great Gatsby'));
+  render(<RemixStub />);
+
+  await waitFor(() => screen.findByText('The lord of the rings'));
 });
