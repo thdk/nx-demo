adb810b676eda7d8b88e910f216f04908cf427f2
Thomas Dekiere <thomas.dekiere@inthepocket.com>
2024-12-30 09:43:56 +0100

    feat: add hello api app

---
+++ apps/hello-api/src/main.ts
@@ -0,0 +1,3 @@
+import { bootstrap } from '@nx-demo/nest-bootstrap';
+import { AppModule } from '@nx-demo/hello-api-lib';
+bootstrap(AppModule);
---
+++ libs/hello-api-lib/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'hello-api-lib',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/hello-api-lib',
+};
